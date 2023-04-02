d251c94cb0d118e7895c25eea6ac1351e40b2afa
Chetan <chetan.sawle@trigensoft.com>
2023-04-02 15:12:22 +0530

    new

--- user/app/Views/layout/navbar.php
+++ user/app/Views/layout/navbar.php
@@ -12,9 +12,29 @@
     <div class="dropdown_menu">
       <li><a href="<?php echo base_url('index.php/home');?>">Home</a></li>
       <li><a href="<?php echo base_url('index.php/about_us');?>">About</a></li>
-      <li><a href="<?php echo base_url('index.php/career');?>">Careers</a></li>
-      <li><a href="<?php echo base_url('index.php/resource');?>">Resources</a></li>
+      <li>
+        <a href="#">Services</a>
+        <ul >
+          <li><a href="#" style="color:#fff;">IT Staffing</a></li>
+          <li><a href="#" style="color:#fff;">IT Solutions</a></li>
+          <li><a href="#" style="color:#fff;">Product Development</a></li>
+          <li><a href="#" style="color:#fff;">HTD</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">Products</a>
+        <ul >
+          <li><a href="#" style="color:#fff;">Qlikdeals</a></li>
+          <li><a href="#" style="color:#fff;">Good Governance</a></li>
+          <li><a href="#" style="color:#fff;">Legacy</a></li>
+          <li><a href="#" style="color:#fff;">Product_1</a></li>
+          <li><a href="#" style="color:#fff;">Product_2</a></li>
+          <li><a href="#" style="color:#fff;">Product_3</a></li>
+        </ul>
+      </li>
       <li><a href="<?php echo base_url('index.php/partner');?>">Partners</a></li>
-      <li><a href="">Contact</a></li>
+      <li><a href="<?php echo base_url('index.php/resource');?>">Resources</a></li>
+      <li><a href="<?php echo base_url('index.php/career');?>">Careers</a></li>
+      <li><a href="<?php echo base_url('index.php/contact');?>">Contact</a></li>
     </div>
   </header>
