25f152fabafd1330db2bcb1ee201b504d30d5b1f
Chetan <chetan.sawle@trigensoft.com>
2023-04-12 23:27:24 +0530

    new

--- user/app/Views/services_submenu/htd.php
+++ user/app/Views/services_submenu/htd.php
@@ -1,43 +1,115 @@
 <div class="banner_div">
-		<img class="banner_img" src="<?php echo base_url('assets/img/career/section_1/hero_image.jpg');?>">
+		<img class="banner_img" src="<?php echo base_url('assets/img/HTD/hero/banner_web.jpg');?>">
 	</div>
 
-	<section class="career_sec2">
+	<section class="white_bg">
+		<img class="sec_2_dot_dtd" src="<?php echo base_url('assets/img/HTD/section_2/dots_pattern.png');?>">
 		<div class="container ">
+			<div class="title-head">Let's unravel our all-new HTD Model</div>
+			<div class="desc_2">
+				In addition to skills like artificial intelligence, software implementation, cyber security, data science, automation & outstanding technical expertise, all IT enterprises demand soft skills as prerequisites.
+			</div>
+			<div class="desc_2">
+				Fixity aims to fix this staffing concern with its new Hire. Train. Deploy model. Let's understand this three-level screening model in detail.
+			</div><br><br>
 			<div class="row">
 				<div class="col-md-6">
-					<img class="w-100 section_space" src="<?php echo base_url('assets/img/career/section_2/philosophy_image.png');?>">
+					<img class="w-100 " src="<?php echo base_url('assets/img/HTD/section_2/hire_image.png');?>">
 				</div>
 
 				<div class="col-md-6">
-					<div class="about_title-head section_space">htd</div><br>
+					<div class="quickdeal_title ">Hire</div><br>
+					<p class="quickdeal_title-head_2">
+						We strongly believe that every Hire counts!</p>
 					<p class="para_about">
-						At Fixity, we are committed to creating a culture of faith, trust, and growth. Our work culture is driven by the discipline of courage, honesty & transparency. We help our employees feel connected, engaged, and aligned with the company's vision to become a global leader in IT staffing and product outsourcing.</p>
+						At the first level, we conduct interviews and shortlisting to zero in on just the type of talent the client is looking for. This screening process helps in filtering out the best candidates who match the requirements of the client.
+					</p>
 					<p class="para_about">
-						We have an enriching custom of celebrating the small wins of our employees to boost their motivation and empower them. We encourage clear communication and foster a safe environment for our employees to share their ideas, views, and opinions. We are committed to the safety of our workplace and true to our philosophy of 'we deliver what we promise' to both our clients and employees.
+						We have partnered with hundreds of government and private colleges in India to pick out the best pool of candidates from the IT arena. We make use of innovative assessment and profiling tools & techniques to filter out the best of the best candidates for the client's requirements.
 					</p>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<section class="career_sec3">
-		<div class="container ">
-			<div class="title-head">Fixity Life</div>
-			<div class="desc_2">
-						Fixity has its presence across three continents currently and we are on a mission to build a global footprint. Join us in this mission and start doing work that matters.
+	<section class="htd_sec3">
+		<div class="quickdeal_title htd_text">Deploy</div>
+		<div class="htd_white_text">
+			We have a decade of training expertise
+		</div>
+			<div class="desc_2_htd">
+				Once we have identified the best pool of candidates in the computer science arena, next comes training them. We get the prescribed skills requirements from the client and train the shortlisted candidates in those technical requirements. Some of the specific skill sets we hold technical expertise in are -
 			</div>
-			<div class="desc_2">
-						At Fixity Tech, you get the chance to be part of an organization that fosters an environment conducive to growth and helps you achieve your professional goals.
+			<img class="w-100 htd_img" src="<?php echo base_url('assets/img/HTD/section_3/logos_web.png');?>">
+			<div class="desc_3_htd">
+				We also train these candidates in much-required soft skills. Our training experts come with an invaluable decade of experience and are equipped to offer experiential learning to the candidates.
 			</div>
-			<div class="desc_2">
-						Our team prioritizes innovation and always seeks to push the boundaries of IT staffing, product outsourcing, and more. We thrive on knowledge exchange and work together to deliver the best services to our clients. No matter who you are or where you're from, your growth is always our priority..
-			</div>
-
-			<div>
-				<img class="w-100" src="<?php echo base_url('assets/img/career/section_3/image_collage.png');?>">
-			</div>
-		</div><br><br><br>
 	</section>
 
-	
+	<section class="white_bg">
+		<img class="sec_4_dot_dtd" src="<?php echo base_url('assets/img/HTD/section_4/dots_pattern.png');?>">
+		<div class="container ">
+			<div class="row padding_b-5">
+				<div class="col-md-6 col-12">
+					<img class="w-100 section_space" src="<?php echo base_url('assets/img/HTD/section_4//deplot_image.png');?>">
+				</div>
+
+				<div class="col-md-6 col-12">
+					<div class="quickdeal_title section_space">Deploy</div><br>
+					<p class="quickdeal_title-head_2">
+						The final decision is collaborative and the client has the final say</p>
+					<p class="para_about">
+						At the first level, we conduct interviews and shortlisting to zero in on just the type of talent the client is looking for. This screening process helps in filtering out the best candidates who match the requirements of the client.After the successful completion of the training, candidates ore fully ready for the project or technical requirements. The client now conducts the final assessment of the cordidotes. Since these condicates are rigorously trainec, there remains a minor scope of error. Due to this, the bounce rate remors minimo. We ciso train around 20% of curdideles as buffers to ensure precision in the torget achievement assigned by the client. We also do this to provice a broader and more accurate selection of candidates. We have three-iale engagement models for the cleoloyment
+					</p>
+				</div>
+
+				<div class="col-md-4 col-12">
+					<div>
+						<img class="" src="<?php echo base_url('assets/img/HTD/section_4/limited_support.png');?>">
+					</div>
+					<div class="delpoy_text_yellow">
+						Contract Staffing for <br>limited support
+					</div>
+					<div class="para_about">
+						We offer hiring for skilled candidates on a contrect oasis. We stick to the specific rising contractual terms and fulfill the client Mining & storing requirements with efficacy
+					</div>
+				</div>
+				<div class="col-md-4 col-12">
+					<div>
+						<img class="" src="<?php echo base_url('assets/img/HTD/section_4/long_support.png');?>">
+					</div>
+					<div class="delpoy_text_yellow">
+						Contract to hire staffing for<br> specific support
+					</div>
+					<div class="para_about">
+						We support our clients for their unique needs like short-term hiring assistance and fulfilling seasonal or temporary hiring requirements. Temporarily hired employees can be appointed permanently based on the client's discretion.
+					</div>
+				</div>
+				<div class="col-md-4 col-12">
+					<div>
+						<img class="" src="<?php echo base_url('assets/img/HTD/section_4/long_support.png');?>">
+					</div>
+					<div class="delpoy_text_yellow">
+						Permanent staffing for <br>long-term support
+					</div>
+					<div class="para_about">
+						We employ systematic research methods and conduct an in-depth analysis to hire the best & exceptional human assets for our clients.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="htd_sec5">
+		<div class="quickdeal_title htd_text_yellow">A revolutionary recruitment model for comprehensive IT staffing support</div>
+			<div class="para_about htd_text_white">
+				Fixity HTD Model adds value to your enterprise. It is a cost-effective method with zero investment in hiring a team and guarantees you trained & skilled professionals
+			</div>
+	</section>
+	<div class="contact_box">
+		<div class="about_contact">
+			Want to know more <br>
+			<label class="about_label">about this model?</label> <button class="about_btn">Contact Us! &nbsp; &gt;</button>
+		</div>
+	</div>
